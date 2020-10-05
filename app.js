10956a1edc64f31c85136b7ffa5bc27534a429e9
PiranavaA <45345607+PiranavaA@users.noreply.github.com>
2020-10-04 20:56:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var num1 = document.getElementById("num1");
 var num2 = document.getElementById("num2");
 var num3 = document.getElementById("num3");
+// Hello, Piranava
 
 
 num1.addEventListener('input', (e) => {
