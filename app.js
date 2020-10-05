f8b0c55cfc9661ca3ba7f3d47ea9dbeb91d07730
RamAbey <Ram.Abey2001@berkeley.edu>
2020-10-05 15:05:13 -0400

    updates

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDLGhKNFhqZpqE1gnVEBvZrxxJd9PMKN8Q",
+    authDomain: "streakio.firebaseapp.com",
+    databaseURL: "https://streakio.firebaseio.com",
+    projectId: "streakio",
+    storageBucket: "streakio.appspot.com",
+    messagingSenderId: "245556748101",
+    appId: "1:245556748101:web:808101e237bb0c400c282c",
+    measurementId: "G-SETJXC9KR4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const auth = firebase.auth();
 
+auth.onAuthStateChanged(user =>{
+    console.log("user info", user);
+    if(user) {
+        // Handle user information
+    } else {
+        window.location = 'login.html'
+    }
+})
+
+document.querySelector('header .sign-out-btn').addEventListener('click', () => {
+    firebase.auth().signOut().then(function() {
+        console.log("User signed out successfully.")
+      }).catch(function(error) {
+        console.log('Error occured while signing out.')
+      });
+})
 
 var num1 = document.getElementById("num1");
 var num2 = document.getElementById("num2");
 var num3 = document.getElementById("num3");
-
 
 
 num1.addEventListener('input', (e) => {
--- login.js
+++ login.js
@@ -1,13 +1,24 @@
-
-
-// PASTE FIREBASE CONFIG HERE
-const firebaseConfig = ""
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDLGhKNFhqZpqE1gnVEBvZrxxJd9PMKN8Q",
+    authDomain: "streakio.firebaseapp.com",
+    databaseURL: "https://streakio.firebaseio.com",
+    projectId: "streakio",
+    storageBucket: "streakio.appspot.com",
+    messagingSenderId: "245556748101",
+    appId: "1:245556748101:web:808101e237bb0c400c282c",
+    measurementId: "G-SETJXC9KR4"
+};
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 const auth = firebase.auth();
 
 signInBtn = document.getElementById('sign-in-btn')
 signUpBtn = document.getElementById('sign-up-btn')
+
+LoginErrorMsg = document.querySelector('.login-wrapper .error-msg')
 
 signUpBtn.addEventListener('click', (e) => {
     const email = document.querySelector('.login-wrapper #email').value
@@ -18,8 +29,11 @@ signUpBtn.addEventListener('click', (e) => {
         console.log("Successfully signed up", Credential.user);
     }).catch(error => {
         console.log("error",error);
+        LoginErrorMsg.innerHTML = `${error.message}`
+        LoginErrorMsg.style.display = 'block'
     });
 })
+
 signInBtn.addEventListener('click', (e) => {
     const email = document.querySelector('.login-wrapper #email').value
     const password = document.querySelector('.login-wrapper #password').value
@@ -29,6 +43,8 @@ signInBtn.addEventListener('click', (e) => {
       console.log("Logged In successfully", Credential);
     }).catch(error=>{
       console.log("error",error);
+      LoginErrorMsg.innerHTML = `${error.message}`
+      LoginErrorMsg.style.display = 'block'
     });
 })
 
